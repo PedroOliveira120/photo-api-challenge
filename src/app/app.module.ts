1fcb1d0d4437aa8cd6150930e43928793cf095d5
pedroliveirastb <pedro.oliveira@spotperfectable.com>
2022-11-13 23:57:09 +0000

    szdfs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ImageSearchComponent } from './image-search/image-search.component';
+import { ImageCardComponent } from './image-search/image-card/image-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSearchComponent,
+    ImageCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
